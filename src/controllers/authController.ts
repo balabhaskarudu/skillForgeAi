93adbc027d918f281c7d5fba1965ae425eec5088
Somana Bala Bhaskarudu <balasomana@Somanas-MacBook-Pro.local>
2026-06-17 17:10:39 +0530

    feat: implement login API with JWT authentication

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { registerUser } from '../services/authService';
+import { loginUser } from '../services/authService';
 
 export const register = async (
   req: Request,
@@ -22,6 +23,35 @@ export const register = async (
       error instanceof Error ? error.message : 'Internal Server Error';
 
     res.status(400).json({
+      success: false,
+      message,
+    });
+  }
+};
+export const login = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await loginUser(req.body);
+
+    res.status(200).json({
+      success: true,
+      message: 'Login successful',
+      token: result.token,
+      data: {
+        id: result.user._id,
+        name: result.user.name,
+        email: result.user.email,
+      },
+    });
+  } catch (error) {
+    const message =
+      error instanceof Error
+        ? error.message
+        : 'Internal Server Error';
+
+    res.status(401).json({
       success: false,
       message,
     });
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,8 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import bcrypt from 'bcryptjs';
 import User, { IUser } from '../models/User';
+import jwt from 'jsonwebtoken';
 
 interface RegisterUserInput {
   name: string;
+  email: string;
+  password: string;
+}
+interface LoginUserInput {
   email: string;
   password: string;
 }
@@ -27,4 +35,45 @@ export const registerUser = async (
   });
 
   return user;
+};
+
+export const loginUser = async (
+  userData: LoginUserInput
+) => {
+  const { email, password } = userData;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Invalid email or password');
+  }
+
+  const isPasswordMatched = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+  if (!isPasswordMatched) {
+    throw new Error('Invalid email or password');
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET as string,
+  {
+    expiresIn: '7d',
+  }
+    // process.env.JWT_SECRET as string,
+    // {
+    //   expiresIn: process.env.JWT_EXPIRES_IN || '7000',
+    // }
+  );
+
+  return {
+    token,
+    user,
+  };
 };
